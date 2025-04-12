325786bdb24091b5adddae402448180496bf8769
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-04-12 20:08:49 +0200

    add commit

---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <Header />
+      <h1>Witaj w sklepie Greenlandia</h1>
+      <p>Wkrótce znajdziesz tutaj nasze produkty!</p>
+    </div>
+  );
+};
+
+export default HomePage;
