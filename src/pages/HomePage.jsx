6d82ab35c9ada9ed36cb5b30061b017f1f1f3af8
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2025-04-13 18:16:56 +0200

    add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,53 @@
 import React from "react";
 import Header from "../components/Header";
-import ImageSlider from "../components/ImageSlider"; // <-- dodajesz ten import
+import ImageSlider from "../components/ImageSlider";
 import ProductCard from "../components/ProductCard";
 
+const products = [
+  {
+    id: 1,
+    image: "./images/Natka.jpg",
+    title: "Natka pietruszki",
+    description: "Naturalne kuchenne mydło do rąk.",
+    price: "24,99",
+  },
+  {
+    id: 2,
+    image: "./images/Fr_Lawenda.jpg",
+    title: "Francuska lawenda",
+    description: "Naturalny płyn uniwersalny Francuska lawenda",
+    price: "19.99",
+  },
+  {
+    id: 3,
+    image: "./images/Wind.jpg",
+    title: "Wind",
+    description: "Naturalny balsam do ciała Wind odprężenie",
+    price: "12.99",
+  },
+];
+
 const HomePage = () => {
+  const handleAddToCart = (productTitle) => {
+    alert(`Dodano "${productTitle}" do koszyka!`);
+  };
+
   return (
     <div className="home-page">
       <Header />
-      <ImageSlider /> {/* <-- slider zaraz pod nagłówkiem */}
-      <ProductCard />
+      <ImageSlider />
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            description={product.description}
+            price={product.price}
+            onAddToCart={() => handleAddToCart(product.title)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
